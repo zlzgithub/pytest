64f577065ca742cb0ed94c9e5744c175736dc5c1
zlzgithub <zenglz.jx@gmail.com>
2018-09-20 22:36:09 +0800

    update mytime.py

--- mytime.py
+++ mytime.py
@@ -1,17 +1,10 @@
 # coding=utf-8
 
 import requests
-import base64
-import re
 import json
-
-
-def get_basic_auth_str(username, password):
-    temp_str = username + ':' + password
-    bytes_string = temp_str.encode(encoding="utf-8")
-    encode_str = base64.b64encode(bytes_string)
-    # decode_str = base64.b64decode(encode_str)
-    return 'Basic ' + encode_str.decode()
+import re
+import uinf
+from bs4 import BeautifulSoup
 
 
 def parse_cookies(str_cookies=''):
@@ -22,6 +15,29 @@ def parse_cookies(str_cookies=''):
         _cookies[str(mat.group(1)).strip()] = str(mat.group(2)).strip()
 
     return _cookies
+
+
+def parse_headers(str_headers='', f_headers='headers.txt'):
+    _str_headers = str_headers
+    _headers = {}
+    if not _str_headers:
+        with open(f_headers, 'r') as fp:
+            _str_headers = fp.read()
+
+    h_patt = re.compile('(.*?):(.*)')
+    for h_line in str(_str_headers).splitlines():
+        # print h_line
+        mat = h_patt.match(h_line)
+        if mat:
+            # print mat.groups()
+            # noinspection PyBroadException
+            try:
+                # _headers[mat.group(1)] = urllib.unquote(mat.group(2))
+                _headers[str(mat.group(1)).strip()] = str(mat.group(2)).strip()
+            except:
+                continue
+
+    return _headers
 
 
 def convert_t(t):
@@ -49,7 +65,7 @@ def cal_tsum(t):
     return t_s, 1, t[1] - t[0]
 
 
-def main(ym):
+def get_time(ym, se):
     url = "http://psa.isoftstone.com/api/Attendance/Home/HomeApi/GetAttendanceList"
     payload = "YearMonth=%s" % ym
 
@@ -57,10 +73,7 @@ def main(ym):
         'accept': "application/json, text/javascript, */*; q=0.01",
         'accept-encoding': "gzip, deflate",
         'accept-language': "zh-CN",
-        'authorization': "87e2e0de1aafca8d9afc460392ad2240106b352d",
         'cache-control': "no-cache",
-        'connection': "Keep-Alive",
-        'content-length': "17",
         'content-type': "application/x-www-form-urlencoded",
         'host': "psa.isoftstone.com",
         'key': "Attendance_Home_MyAttendance_View_GetAttendanceList",
@@ -69,17 +82,10 @@ def main(ym):
         'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134",
         'x-requested-with': "XMLHttpRequest",
     }
-	
-	token = get_basic_auth_str(name, passwd)
-    token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-    # headers['authorization'] = token
 
-    str_cookies = ".iPSA=5AAF8FC1B8989A57E1581C67E8369549B22849B58EE57B2CA0B129F3CAF9FF2B573915C92C497BA9535E36C21A778445E551FEC8B1D37079AB597DB836F86BD1B584C486A2816A456AEE7E64ACE14923; DomainName=lzzengc; UserName=Ek/4jFHJbg0=; IsJV=True; idsrv=CfDJ8MaMGgRH1RtDsbhcSI5XbTzF9VlXMvPGJX-rfQa1x2wja7VEDxBr7PTUtBRiyg9oojNPWAjfmp6bbeYdWTfpI9iAeL5uP7Ny1yyySoQLsIf_ugNAuWD4LDpwPaVEvuwgc97cH4CwvfSzf15i1K6HWNYfJxap36E6OGzVeaxbfSQo0QIyedH48gcAnmJv_LN7H6xxNhyD3Cr3plGDPAZsYj8yb1EdHqZMhUQbd7PjzgrypvhiULSeCK1DAD4lEBb_sMImUtofMDQlBU1i4bSG5XJC9TU4rjmJbvP_3Dqurf7A1Avaxot6-jIlLLmen41B_p3Gh32cA4tnqp1VCMf_CY4EjJogo5bKVRhw4eZADfroHszueq2HBVVv5JNwxE_3GIFY9CxyHR50AirdBC6e-Id83g8UP8prXG8oPKFBrk_1oD-61GFBCupl0Dp3xNvJYsCtIxtnsGDCfnD0iXR8HYB2mEJRrVMQrhnp0BXln8koHeGS06HdrkSMFD5PFDYZ5yBmJHrpNMag3ENuPVA8LrduaHb0M5f95f5krHNVR9co43gH9ts9qKLRTYkswzqG4OHUE6Mc_WOJbXK3gtS_93s; SESSION_THEME_KEY=red"
-    cookies = parse_cookies(str_cookies)
-    
-    cookies['token'] = token
-    response = requests.request("POST", url, data=payload, headers=headers, cookies=cookies)
-    time_dic = json.loads(response.content)
+    # response = requests.request("POST", url, data=payload, headers=headers, cookies=cookies)
+    r = se.post(url, data=payload, headers=headers)
+    time_dic = json.loads(r.content)
     daily_time = time_dic['Data']['Result']['Days']
 
     t_sum = 0
@@ -97,15 +103,73 @@ def main(ym):
             a_sum += a
 
     print
-    print "************* %s月工时统计 (%2d天) *************" % (mon, d_sum)
-    print "绝对时长：%16s %16s" % (str("%d Min" % a_sum), str("%0.1f H" % (a_sum * 1.0 / 60)))
-    print "应有工时：%16s %16s" % (str("%s Min" % (d_sum * 480)), str("%0.1f H" % (8.0 * d_sum)))
-    print "有效工时：%16s %16s" % (str("%d Min" % t_sum), str("%0.1f H" % (t_sum * 1.0 / 60)))
-    print "有效均值：%16s %16s" % (str("%d Min" % (t_sum / d_sum)), str("%0.1f H" % (t_sum * 1.0 / d_sum / 60)))
-    print "有效均差：%16s %16s" % (str("%s Min" % (t_sum / d_sum - 480)), str("%0.1f H" % ((t_sum / d_sum - 480) * 1.0 / 60)))
+    print "************* %s (%2d D) *************" % (ym, d_sum)
+    print "%12s %12s %12s" % ("Absolute:", str("%d Min" % a_sum), str("%0.1f H" % (a_sum * 1.0 / 60)))
+    print "%12s %12s %12s" % ("Needed:", str("%s Min" % (d_sum * 480)), str("%0.1f H" % (8.0 * d_sum)))
+    print "%12s %12s %12s" % ("Valid:", str("%d Min" % t_sum), str("%0.1f H" % (t_sum * 1.0 / 60)))
+    print "%12s %12s %12s" % ("Average:", str("%d Min" % (t_sum / d_sum)), str("%0.1f H" % (t_sum * 1.0 / d_sum / 60)))
+    print "%12s %12s %12s" % ("Diff:", str("%s Min" % (t_sum / d_sum - 480)), str("%0.1f H" % ((t_sum / d_sum - 480) * 1.0 / 60)))
 
 
-# year_mon = raw_input("输入月份，如2018-07：\n")
+url = 'https://passport.isoftstone.com/?DomainUrl=http://ipsapro.isoftstone.com&ReturnUrl=%2fPortal%2f'
+# url = urllib.unquote(url)
+headers = parse_headers()
+cookies = {}
+payload = {"DomainUrl": "",
+           "emp_DomainName": uinf.user,
+           "emp_Password": uinf.passwd,
+           "returnUrl": ""
+           }
+
+session = requests.Session()
+r = session.post(url, headers=headers, data=payload)
+print r.status_code, "POST", url
+
+url = "http://ipsapro.isoftstone.com/portal/Special/hwkq"
+headers = {'upgrade-insecure-requests': "1",
+           'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+           'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+           'referer': "http://ipsapro.isoftstone.com/Portal",
+           'accept-encoding': "gzip, deflate",
+           'accept-language': "zh-CN,zh;q=0.8",
+           'cache-control': "no-cache",
+           }
+
+r = session.request("GET", url, headers=headers)
+print r.status_code, "GET", url
+token = ""
+soup = BeautifulSoup(r.content, "html.parser")
+tk = soup.find(id='token')
+if tk:
+    token = tk.get('value')
+
+url = "http://psa.isoftstone.com/Common/User/UserView/IntegrateLogin"
+payload = {"callbackUrl": "http://psa.isoftstone.com/Attendance/Home/HomeView/Index",
+           "tenant": "PSA",
+           "token": token,
+           "verifyUrl": "http://timesheet.isoftstone.com:9119/api/PSA/CheckLogin"
+           }
+
+headers = {"origin": "http://ipsapro.isoftstone.com",
+           "upgrade-insecure-requests": "1",
+           "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+           "content-type": "application/x-www-form-urlencoded",
+           "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+           "referer": "http://ipsapro.isoftstone.com/portal/Special/hwkq",
+           "accept-encoding": "gzip, deflate",
+           "accept-language": "zh-CN,zh;q=0.8",
+           "cache-control": "no-cache"
+           }
+
+r = session.request("POST", url, data=payload, headers=headers)
+print r.status_code, "POST", url
+cookies = parse_cookies(r.request.headers['Cookie'])
+print "Cookies:"
+print json.dumps(cookies, indent=2)
+print
+
+year_mon = raw_input("输入月份，如\"2018-07\"：\n")
 year_mon = "2018-09"
-main(year_mon)
-
+get_time(year_mon, session)
+year_mon = "2018-08"
+get_time(year_mon, session)
